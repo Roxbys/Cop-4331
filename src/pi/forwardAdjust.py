69513a4216cfbca0b33b07187696d87d0e194bef
Michael Pfeiffer <medispawn@gmail.com>
2017-06-29 14:37:23 -0400

    several changes (see extended description)
    Created functions to check the green state as well as to calibrate the robot path.  Search patterns start at the opposite side of the previous search and increasing oscillate outwards until green is found again.  Search time doubles each time the robot must search a different direction.  Search fails after an oscillation exceeds 5 seconds.  Now checking for green during search period instead of after to avoid overcompensation.   
    
    In order to avoid setting the motor speeds constantly (which results in program failure) there are now state variables.
    
    Added additional code formatting to make it easier to read.

--- src/pi/forwardAdjust.py
+++ src/pi/forwardAdjust.py
@@ -1,37 +1,72 @@
 # small test to ensure forward movement on detection of green color by rgb sensor
-
 from a_star import AStar
 import Adafruit_TCS34725
 import smbus
 import time
 
-main()
-  
+# recalibrate the robot onto the path
+def calibrateDirection(a_star, tcs, mode, searchTime, turnWheelSpeed):
+  adjustTime = time.time()
+  # search left
+  if(mode == 0):
+    a_star.motors(turnWheelSpeed, 100)
+  # search right
+  elif(mode == 1):
+    a_star.motors(100, turnWheelSpeed)
+  while((time.time() - adjustTime) < searchTime):
+    if(checkIfGreen(tcs)):
+      break
+  # end while 
+# end calibrateDirection()
+
+def checkIfGreen(tcs):
+  r, g, b, c = tcs.get_raw_data()
+  return (g > r and g > b)
+# end checkIfGreen()
+ 
 def main():
+  # create romi and rgb sensor objects
   a_star = AStar()
   tcs = Adafruit_TCS34725.TCS34725()
   tcs.set_interrupt(False)
-  
-  #initial state of green
-   if (g > r and g > b):
+ 
+  # initial states
+  isGoingForward = False
+  searchFailed = False
+  # used to switch left/right search (left: 0, right: 1)
+  toggleSearchDirection = 0 
+ 
+  while (not searchFailed):
+    isGreen = checkIfGreen(tcs)
+    # if green is found and not already moving, move forward
+    if (isGreen and (not isGoingForward)):
       a_star.motors(100, 100)
-
-  while (True): 
-    r, g, b, c = tcs.get_raw_data()
-    while(g < r or g < b):
-      adjustTime = time.time()
-      a_star.motors(50, 100)
-      while((time.time() - adjustTime) < 0.5):
-        pass 
-      r, g, b, c = tcs.get_raw_data()
-      if(g > r and g > b): 
-        break
-      adjustTime = time.time()
-      a_star.motors(100, 50)
-      while((time.time() - adjustTime) < 1):
-        pass
-      a_star.motors(100, 100)
+      isGoingForward = True
+    # unnecessary to update motor speed if already going forward
+    elif(isGreen and isGoingForward):
+      pass
+    # green not found, start search
+    else:
+      isGoingFoward = False
+      # default search time for initial stray
+      searchTime = .2 
+      while(not isGreen):
+        calibrateDirection(a_star, toggleSearchDirection, searchTime, 40)
+        toggleSearchDirection = toggleSearchDirection ^ 1
+        isGreen = checkIfGreen(tcs)
+        # don't allow an oscillation to exceed 5 seconds
+        if(searchTime > 5 and (not isGreen)):
+          searchFailed = True
+          break
+        else:
+          # double search time on opposite side
+          searchTime *= 2
+      # end while
+  # end while
   
   # Enable interrupts and put the chip back to low power sleep/disabled.
   tcs.set_interrupt(True)
   tcs.disable()
+#end main()
+
+if __name__ == "__main__": main()
