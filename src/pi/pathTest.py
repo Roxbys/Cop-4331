4f2ee151051541dedb1154cb8fcd5766a7e45a28
Shawn Roxby <roxbys@knights.ucf.edu>
2017-07-06 21:06:41 +0000

    Update pathTest.py

--- src/pi/pathTest.py
+++ src/pi/pathTest.py
@@ -1,19 +1,36 @@
-# simple test that utilizes the romi class
+#simple test that utilizes the romi class
 # move forward on green until lost
 from romi import *
 
 def main():
-    romi = Robot()
-    romi.turnOffInterruptsRGB()
-    
-    while(not romi.isLost):
-      romi.followPath()
-        
-    if(romi.isLost):
-        romi.giveUp()
-      
-    romi.turnOnInterruptsRGB()
-    romi.disableRGB()
+  romi = Robot()
+  romi.turnOffInterruptsRGB()
+  '''
+  while(True):
+    if(romi.checkGreen()):
+      romi.goForward()
+    else:
+      romi.calibrate()
+  romi.stop()
+  '''
+  '''
+  romi.turn90Left()
+  time.sleep(2)
+  romi.turn90Right()
+  time.sleep(2)
+  romi.goForward()
+  time.sleep(3)
+  romi.stop()
+  time.sleep(2)
+  romi.turn90Left()
+  romi.turn90Left()
+  time.sleep(3)
+  romi.goForward()
+  time.sleep(2)
+  romi.stop()
+  ''' 
+  romi.turnOnInterruptsRGB()
+  romi.disableRGB()
 #end main
     
 if __name__ == "__main__": main()
