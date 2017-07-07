a167e7bdc67d203ea3374e880fbf0eb9ee7d0735
Shawn Roxby <roxbys@knights.ucf.edu>
2017-07-07 00:20:03 -0400

    Add files via upload

--- src/pi/romi.py
+++ src/pi/romi.py
@@ -2,13 +2,14 @@ from a_star import AStar
 import Adafruit_TCS34725
 import smbus
 import time
+import math
 
 class Robot:
   def __init__(self):
     self.isLost = False
     self.nextSearchDirection = 0  # (left: 0, right: 1)
     self.veerSpeed = 40
-    self.maxSpeed = 200
+    self.maxSpeed = 100
     self.aStar = AStar()
     self.tcs = Adafruit_TCS34725.TCS34725()
     self.initialLeftCount = self.aStar.read_encoders()[0]
@@ -18,6 +19,10 @@ class Robot:
   def checkGreen(self):
     r, g, b, c = self.tcs.get_raw_data()
     return (g > r and g > b)
+
+  def checkRed(self):
+    r, g, b, c = self.tcs.get_raw_data()
+    return (r > g and r > b)
   
   # set motor speeds
   def motors(self, lspeed, rspeed):
@@ -77,6 +82,40 @@ class Robot:
   def disableRGB(self):
     self.tcs.disable()
 
+  def turnLeft(self,count):
+    while(self.readEncoders()[0]%744 > (-1)*count and self.readEncoders()[1]%744 < count):
+      if(self.checkGreen()):
+        return 1
+        break
+      self.motors(0, self.maxSpeed)
+      self.stop()
+      return 0
+
+  def turnRight(self,count):
+    while(self.readEncoders()[0]%744 < count and self.readEncoders()[1]%744 > (-1)*count):
+      if(self.checkGreen()):
+        return 1
+        break
+      self.motors(self.maxSpeed, 0)
+      self.stop()
+      return 0
+
+  def changeCount(self,count):
+    if(count != 0):
+      count = count / 2
+		
+  def calibrate(self):
+    count = 744
+    while(self.checkGreen() == False):
+      if(self.turnLeft(count)):
+        pass
+      elif(self.turnRight(count)):
+        pass
+      self.changeCount(count)
+    # end while
+    self.goForwardtwo()
+
+
   def turn90Left(self):
     initEncoders = self.readEncoders()
     while(    self.readEncoders()[0] > initEncoders[0] - 744
@@ -90,29 +129,41 @@ class Robot:
           and self.readEncoders()[0] < initEncoders[0] + 744):
       self.motors(self.maxSpeed, self.maxSpeed * (-1))
     self.stop()
-  
-  def goForward(self):
+ 
+  def goForwardtwo(self):
     self.motors(self.maxSpeed, self.maxSpeed)
     x = self.maxSpeed
-    if(self.readEncoders()[1] > self.readEncoders()[0])
-      while(self.readEncoders()[1]>self.readEncoders()[0])
-        self.motors(x--,self.maxSpeed)
-        self.sleep(.01)
-      x = self.maxSpeed
-      while(self.readEncoders()[1]>self.readEncoders()[0])
-        self.motors(self.maxSpeed,x--)
-        time.sleep(.01)
+    diff = math.fabs(self.readEncoders()[1] - self.readEncoders()[0]) 
+    while(diff > 2):
+      diff = math.fabs(self.readEncoders()[1] - self.readEncoders()[0]) 
+      x-=1
+      if(self.readEncoders()[1] > self.readEncoders()[0]):
+        self.motors(self.maxSpeed,x)
+        time.sleep(1.0/1000.0)
+      elif(self.readEncoders()[0] > self.readEncoders()[1]):
+        self.motors(x,self.maxSpeed)
+        time.sleep(1.0/1000.0)
+    self.motors(self.maxSpeed, self.maxSpeed)
 
+  '''   
   def calibrate(self):
     while(self.checkGreen() == False):
       for x in range(100, -20, -1):
         self.motors(100 if x+40>100 else x+40,x)
       
+  '''
+
+  def adjustIntersection(self):
+    saveEncoders = self.readEncoders()
+    while(self.readEncoders()[0] - saveEncoders[0] < 648
+          or self.readEncoders()[1] - saveEncoders[1] < 648):
+      self.goForwardtwo()    
       
   # recalibrate the robot onto the path
   def calibrateDirection(self):
     calibrated = False
     turnAmount = 200 # initial encoder count
+    inRed = False
     
     while(self.checkGreen() == False):
       # turn the robot one direction 
@@ -125,11 +176,25 @@ class Robot:
       # wait to see if direction was correct  
       while(self.readEncoders()[self.nextSearchDirection] < turnAmount):
         if(self.checkGreen()):
+          self.adjustIntersection()
+          while(not self.checkGreen()):
+            if(self.nextSearchDirection == 0):
+              self.motors(-100, 100)
+            else:
+              self.motors(100, -100)
+              
+          slapEncoders = self.readEncoders()
+          
+          if(self.nextSearchDirection == 0):
+            while(self.readEncoders()[0] - slapEncoders[0] < 81):
+              self.motors(-100, 100)
+            self.stop()
+          else:
+            while(self.readEncoders()[1] - slapEncoders[1] < 81):
+              self.motors(100, -100)
+            self.stop()
           break
       # end while
-      turnAmount *= 2  # double max search time for next direction 
-    # end while
   # end calibrateDirection()
-
   
 # end Class Robot()
