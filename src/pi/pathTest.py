8e2e947cc41ccdbf331a9b78e0703c8ff6da5432
Shawn Roxby <roxbys@knights.ucf.edu>
2017-07-20 12:58:58 -0400

    Add files via upload

--- src/pi/pathTest.py
+++ src/pi/pathTest.py
@@ -2,59 +2,72 @@
 # move forward on green until lost
 from romi import *
 import time
+
+romi = Robot()
+romi.turnOffInterruptsRGB()
+
 def main():
-  romi = Robot()
-  romi.turnOffInterruptsRGB()
-  initTime = time.clock()
 
+  #while(True):
+   # romi.printColorInfo()
+    #print(romi.checkPurple())
+  #romi.calibrateColors()
+  if(romi.completeMaze()):
+    print("Holy shit! You found the end!")
+
+  '''
   while(True):
+  
     if(romi.checkGreen()):
-      romi.goForwardtwo()
-    else:
-      romi.calibrateDirection()
-  
-  '''
-  while(True):
-    while(romi.checkGreen()):
-      romi.goForwardtwo()
-
-    if(romi.checkRed()):
+      romi.goForward()
+    elif(romi.checkRed()):
+      print('found red')
+      romi.stop()
+      time.sleep(1)
       romi.adjustIntersection()
-      
-    romi.calibrateDirection()
-
-    if(time.clock() - initTime > 10):
-      break
-  
+      #romi.reset()
+      pathInfo = romi.getValidPaths()
+      #romi.reset()
+      if len(pathInfo) == 1:
+         print("deadend")
+         romi.turnCounts('left', pathInfo[0][1])
+         romi.calibrateTwo()
+      else:
+        romi.turnCounts('left', 744)
+        romi.stop()
+        time.sleep(1)
+    else:
+      romi.calibrateTwo()
+    romi.reset()
+    '''
   romi.stop()
-  '''
-  
-  '''
-  while(romi.checkGreen()):
-    romi.goForward()
-    romi.printColorInfo()
-    
-  romi.stop()
-  
-
-  romi.turn90Left()
-  time.sleep(2)
-  romi.turn90Right()
-  time.sleep(2)
-  romi.goForward()
-  time.sleep(3)
-  romi.stop()
-  time.sleep(2)
-  romi.turn90Left()
-  romi.turn90Left()
-  time.sleep(3)
-  romi.goForward()
-  time.sleep(2)
-  romi.stop()
-  ''' 
   romi.turnOnInterruptsRGB()
   romi.disableRGB()
+  '''
+  def backTracking():
+    print('found red')
+    romi.stop()
+    time.sleep(1)
+    romi.adjustIntersection()
+    pathInfo = romi.getValidPaths()
+    if not pathInfo:
+      romi.turnCounts('left', 3000)
+      romi.calibrateTwo()
+    else:
+      for i in range (0, if (len(pathInfo) > 0) then len(pathInfo) - 1 else 0):
+        counts = [y for (x, y) in pathInfo if x == i]
+        if not counts:
+          print("no path in direction" + str(i))
+        else:
+          romi.turnCounts('left', 744)
+          romi.calibrateTwo()
+          main()
+   
+    '''
+      
 #end main
     
-if __name__ == "__main__": main()
+if __name__ == "__main__": main()   
+
+
 
