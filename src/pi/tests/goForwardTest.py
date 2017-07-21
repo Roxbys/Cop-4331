158a282ecfb3fffdbfd942002eed9582ce227a0a
MeganHerrera <megan.herrera5@gmail.com>
2017-07-20 21:02:18 -0400

    Update goForwardTest.py

--- src/pi/tests/goForwardTest.py
+++ src/pi/tests/goForwardTest.py
@@ -1,6 +1,6 @@
 #To test if the robot is going in a straight line along a path, we must 
 #first position the robot at the end of the colored tape. 
-#Then we execute the GoForwardTest() function of the code while passing 0 as an argument. 
+#Execute goForwardTest.py in the raspberry pi terminal. 
 #Then we check to see if the Robot goes off the green tape, that it will detect the absence of color and 
 #calibrate itself back to the colored tape. If so then the test passed. 
 #If the Robot never went back to the colored tape then the test failed.
